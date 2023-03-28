da3177e65007f50da819b07127b4b23857f3c813
Raul-Gonzalez-M <raulgonmu@gmail.com>
2023-03-28 13:50:46 +0200

    con todo mezclado

---
+++ Gymware/src/model/Aula.java
@@ -0,0 +1,49 @@
+import java.util.List;
+import java.util.ArrayList;
+import src.model.Actividad;
+
+public class Aula {
+    
+    private int capacidadMaxima;
+    private int capacidadActual;
+    private String nombre;
+    private List<Actividad> clases;
+    
+    public Aula(int capacidadMaxima, String nombre) {
+        this.capacidadMaxima = capacidadMaxima;
+        this.capacidadActual = 0;
+        this.nombre = nombre;
+        this.clases = new ArrayList<>();
+    }
+    
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+    
+    public int getCapacidadActual() {
+        return capacidadActual;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public List<Actividad> getClases() {
+        return clases;
+    }
+    
+    public void agregarClase(Actividad clase) {
+        if (capacidadActual < capacidadMaxima) {
+            clases.add(clase);
+            capacidadActual++;
+        } else {
+            throw new IllegalStateException("El aula está llena");
+        }
+    }
+    
+    public void eliminarClase(Actividad clase) {
+        if (clases.remove(clase)) {
+            capacidadActual--;
+        }
+    }
+}
